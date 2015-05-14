47028544f8bad436bd18ad270aaec053a3be4969
Adam Eberlin <aeberlin@enova.com>
2015-05-14 09:32:47 -0500

    Update with REPL.

--- dino_catalog/source/main.rb
+++ dino_catalog/source/main.rb
@@ -1,5 +1,19 @@
+#!/usr/bin/env ruby
+
+# Configure pry
+require 'pry'
+Pry.config.prompt = proc { |*args| 'DinoCatalog > ' }
+
+# Load project libraries
 PROJECT_ROOT = File.dirname(__FILE__)
 $LOAD_PATH.unshift(PROJECT_ROOT)
 Dir["#{PROJECT_ROOT}/lib/**/**.rb"].each do |file|
   require file
 end
+
+# Setup DinoCatalog
+DinoCatalog = DinoDex::Catalog.new(DinoDex::Dinosaur)
+DinoCatalog.import(*Dir["#{PROJECT_ROOT}/data/**/**.csv"])
+
+# Setup REPL
+DinoCatalog.instance_eval %{ binding.pry }
